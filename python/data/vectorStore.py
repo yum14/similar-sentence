bc15c5f1db251ec3f301e8f72dad08ffb402a21e
yum14 <yumurata1214@gmail.com>
2021-08-09 16:22:59 +0900

    :recycle: firestoreコレクション定義を変更

--- python/data/vectorStore.py
+++ python/data/vectorStore.py
@@ -3,15 +3,18 @@ from .vectorModel import VectorModel
 import uuid
 
 class VectorStore():
+    USERS_COLLECTION_NAME = 'users'
+    VECTORS_COLLECTION_NAME = 'vectors'
+
     def __init__(self, uid):
-        self.collectionName = 'vectors_' + uid
+        self.uid = uid
         self.db = firestore.client()
 
     def add(self, vector: VectorModel):
-        self.db.collection(self.collectionName).document(vector.id).set(vector.to_dict())
+        self.db.collection(self.USERS_COLLECTION_NAME).document(self.uid).collection(self.VECTORS_COLLECTION_NAME).document(vector.id).set(vector.to_dict())
 
     def get(self):
-        docs = self.db.collection(self.collectionName).stream()
+        docs = self.db.collection(self.USERS_COLLECTION_NAME).document(self.uid).collection(self.VECTORS_COLLECTION_NAME).stream()
 
         dict_list = list(map(lambda x: x.to_dict(), docs))
         return list(map(lambda x: VectorModel(x['id'], x['sentence'], x['vector']), dict_list))
