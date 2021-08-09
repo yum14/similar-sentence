ad2c2e173633bcaa576d78bca62ffed47812248a
yum14 <yumurata1214@gmail.com>
2021-08-09 10:14:06 +0900

    :recycle: DBのフィールド名/変数名を変更

--- python/data/vectorModel.py
+++ python/data/vectorModel.py
@@ -1,11 +1,11 @@
 from firebase_admin import firestore
 
 class VectorModel:
-    def __init__(self, id, sentence, vector, createdAt=firestore.SERVER_TIMESTAMP):
+    def __init__(self, id, sentence, vector, created_at=firestore.SERVER_TIMESTAMP):
         self.id = id
         self.sentence = sentence
         self.vector = vector
-        self.createdAt = createdAt
+        self.created_at = created_at
 
     def to_dict(self):
-        return {'id': self.id, 'sentence': self.sentence, 'vector': self.vector, 'createdAt': self.createdAt}
+        return {'id': self.id, 'sentence': self.sentence, 'vector': self.vector, 'created_at': self.created_at}
